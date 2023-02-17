d1c51be21bef4299a7b5756c2f314febf60d1d45
Som <96942390+soumdatta81@users.noreply.github.com>
2023-02-17 09:41:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import numpy as np
+import openai
+import jellyfish
+
+
+openai.api_key = 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
+
+question = "how should I spend my 20s?"
+speakers = []
+convo = []
+
